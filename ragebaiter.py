f570769e09bd83a67f998fcd3d3abb2f56143fd1
Cossolius-Programmer <gamingwithfelix68@gmail.com>
2026-03-31 07:22:00 +0530

    Refactor nerdify function and improve imports
    made random only import the random function, and changed the thing to add the convention I'm now following

--- ragebaiter.py
+++ ragebaiter.py
@@ -1,20 +1,20 @@
-import random
+from random import random
 
 def nerdify(text: str, strength: float) -> str:
     if 0.0 >= strength >= 1.0:
         raise ValueError('Strength must be between 0 and 1')
-    new_text = ''
+    new_text: str = ''
     for char in text:
         if char not in '0123456789':
-            new_text += char.upper() if random.random() < strength else char
+            new_text += char.upper() if random() < strength else char
         else:
             new_text += char
     return new_text
 
 while True:
-    message = input('Enter a message: ')
+    message: str = input('Enter a message: ')
     if message == '': # need to stop program
         print()
         print("Ok we done.")
         break # self close
-    print(nerdify(message, 0.52))
+    print(nerdify(message, 0.52)) # copy paste this to make the other person think you're mocking them
